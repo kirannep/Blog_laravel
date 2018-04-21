829cf7e074d5aba8a6373454c387435f67e71248
kiran <zbvb177@live.rhul.ac.uk>
2018-04-21 14:24:24 +0100

    First laravel project, creating blog website, at this point created webpages, implemented authentication, now working on post so that admin can create, edit or delete the post

---
+++ blogging/resources/views/posts/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+<div class ="container">
+        <div class="jumbotron">
+            <h2>Administrator</h2>
+            
+            <form action = "posting.php" method = "post" class = "form">
+                <div class="row">
+                    <div class="form-group col-xs-5 col-sm-8">
+                        <label>Title</label>
+                        <input type = "text" class = "form-control" name = "title">
+                    </div>
+                </div>
+                      
+                <div class="row">
+                    <div class="form-group col-xs-6 col-sm-10">
+                        <label>Article</label>
+                        <textarea type = "text" class = "form-control"  name = "article-ckeditor" ></textarea>
+                    </div>
+                </div>
+                  
+                <button type="submit" class="btn btn-primary btn-lg">Publish</button>
+                
+            </form>
+                            
+        </div>
+    </div>
+
+    
+
+@endsection
